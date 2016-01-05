9c2428da8dddf49c543aa0835e8243b1942b92c5
Jon Hecht <jhecht@smartassdesign.com>
2016-01-05 18:18:59 -0500

    add culture content

--- inc/quickcontent.php
+++ inc/quickcontent.php
@@ -6,3 +6,11 @@ function symposium_shortcode( $attr ) {
     return ob_get_clean();
 }
 add_shortcode( 'symposium', 'symposium_shortcode' );
+
+
+function culture_shortcode( $attr ) {
+    ob_start();
+    get_template_part( 'quick/pg-culture' );
+    return ob_get_clean();
+}
+add_shortcode( 'culture', 'culture_shortcode' );
